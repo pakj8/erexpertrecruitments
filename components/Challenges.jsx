c7923ca516ad4c52d5ca22a4341c15e1950afca9
Prakash Jha <pjha.prakash0812@gmail.com>
2022-07-12 17:37:32 +0530

    Added alt props in image

--- components/Challenges.jsx
+++ components/Challenges.jsx
@@ -26,6 +26,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757260/Recruitments/ads_xvzwaw.png"
                   name="Ads Sales Manager"
                   bgColor="#2F8F9D"
+                  alt="ER Sales Manager"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -33,6 +34,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757258/Recruitments/seo_myda2e.png"
                   name="SEO"
                   bgColor="#3BACB6"
+                  alt="ER SEO"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -40,6 +42,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757256/Recruitments/content_rfbakz.png"
                   name="Content Syndication Head"
                   bgColor="#82DBD8"
+                  alt="ER Content Syndication Head"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -47,6 +50,7 @@ function Challenges() {
                   bgColor="#B3E8E5"
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757256/Recruitments/coding_lycd47.png"
                   name="Front End Developer"
+                  alt="ER Front End Developer"
                 />
               </div>
             </Row>
@@ -60,6 +64,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757259/Recruitments/user_exnktc.png"
                   name="Marketing Head"
                   bgColor="#2F8F9D"
+                  alt="ER Marketing Head"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -67,6 +72,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757258/Recruitments/process_guxb12.png"
                   name="Procurement Manager"
                   bgColor="#3BACB6"
+                  alt="ER Procurement Manager"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -74,6 +80,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757256/Recruitments/architect_thpi1e.png"
                   name="Head of Architecture"
                   bgColor="#82DBD8"
+                  alt="ER Head Of Architecture"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -81,6 +88,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757256/Recruitments/chef_ecr0sq.png"
                   name="Corporate Chef"
                   bgColor="#B3E8E5"
+                  alt="ER Corporate Chef"
                 />
               </div>
             </Row>
@@ -94,6 +102,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757256/Recruitments/clipboard_pta5oo.png"
                   name="Deputy Manager: E-insurance"
                   bgColor="#2F8F9D"
+                  alt="ER E Insurance"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -101,6 +110,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757258/Recruitments/team_hreizh.png"
                   name="Regional Sale Manager"
                   bgColor="#3BACB6"
+                  alt="ER Regional Sale Manager"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -108,6 +118,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757259/Recruitments/aggregation_othfsm.png"
                   name="Deputy Manager: Web Aggregator"
                   bgColor="#82DBD8"
+                  alt="ER Web Aggregator"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -115,6 +126,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757257/Recruitments/handshake_kxldq5.png"
                   name="Business Development Manager"
                   bgColor="#B3E8E5"
+                  alt="ER Business Development Manager"
                 />
               </div>
             </Row>
@@ -135,6 +147,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757832/Recruitments/edit_ugcjo6.png"
                   name="Chief Manager Underwriting"
                   bgColor="#3BACB6"
+                  alt="ER Chief Manager Underwriting"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -142,6 +155,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757260/Recruitments/social-care_pjmcbm.png"
                   name="Service Officer"
                   bgColor="#82DBD8"
+                  alt="ER Service Officer"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -149,6 +163,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757257/Recruitments/hr_h4cvhf.png"
                   name="HR Manager"
                   bgColor="#B3E8E5"
+                  alt="ER HR Manager"
                 />
               </div>
             </Row>
@@ -162,6 +177,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757932/Recruitments/office_e3wto1.png"
                   name="Relationship Manager- Branch Banking"
                   bgColor="#2F8F9D"
+                  alt="ER Branch Banking"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -169,6 +185,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757260/Recruitments/web-design_t2vpio.png"
                   name="Graphic Designers"
                   bgColor="#3BACB6"
+                  alt="ER Graphic Designers"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -176,6 +193,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656758237/Recruitments/curve_ostrg7.png"
                   name="LUX Designer/Developer"
                   bgColor="#82DBD8"
+                  alt="ER LUX Designer"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -183,6 +201,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757258/Recruitments/interior-design_fair9s.png"
                   name="Interior Draftsman"
                   bgColor="#B3E8E5"
+                  alt="ER Interior Draftsman"
                 />
               </div>
             </Row>
@@ -196,6 +215,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757258/Recruitments/leadership_pd4gua.png"
                   name="Team Leader/AM"
                   bgColor="#2F8F9D"
+                  alt="ER Team Leader and Team Manager"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -203,6 +223,7 @@ function Challenges() {
                   bgColor="#3BACB6"
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757258/Recruitments/paint-palette_oqw5uo.png"
                   name="Editor/Art Director"
+                  alt="ER Editor Director"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -210,6 +231,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757257/Recruitments/executive_faxujf.png"
                   name="Executive Assitants"
                   bgColor="#82DBD8"
+                  alt="ER Executive Assitants"
                 />
               </div>
               <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
@@ -217,6 +239,7 @@ function Challenges() {
                   images="https://res.cloudinary.com/dh7xchikj/image/upload/v1656757257/Recruitments/crm_pihfr2.png"
                   name="Relationship Manager"
                   bgColor="#B3E8E5"
+                  alt="ER Relationship Manager"
                 />
               </div>
             </Row>
